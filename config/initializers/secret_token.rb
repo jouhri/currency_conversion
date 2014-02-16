0e5fdf1e63c4ea95aa9f6b71d8b7245e928d70ae
Oualid <jouhri.oualid@gmail.com>
2014-02-16 19:05:01 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CurrencyConversion::Application.config.secret_key_base = '39bf3f3fc9024bf35ed2fd43b0833a4efb403d337abdc673e555257a1a00735f17c4f4ae1fa2ac5e8c24b2d7489f25439ab6cd1bf029e1ddca76fc9a7d244b7b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CurrencyConversion::Application.config.session_store :cookie_store, key: '_currency_conversion_session'
